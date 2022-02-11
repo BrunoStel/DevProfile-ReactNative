12df93d80d556396d849eb07c436c2df6998be30
Bruno <brunostel@gmail.com>
2022-02-11 15:51:33 -0300

    chore: add styled-components

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+export const Home: React.FunctionComponent = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <Text>Home Page</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
