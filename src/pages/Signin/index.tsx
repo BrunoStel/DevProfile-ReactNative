b2faadb6d4abb454a73ff3741c89d35b3f70ad8e
Bruno <brunostel@gmail.com>
2022-02-13 17:09:30 -0300

    feat: add logo to signin route

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -3,22 +3,36 @@ import { ScrollView } from 'react-native';
 import { Button } from '../../components/Form/Button';
 import { Input } from '../../components/Form/Input';
 import { SigninStyle } from './styles';
+import logo from '../../assets/logo.png';
 
 export const Signin: React.FunctionComponent = () => {
   return (
-    <ScrollView
-      keyboardShouldPersistTaps="handled"
-      contentContainerStyle={{ flex: 1 }}
-    >
-      <SigninStyle.Container>
-        <SigninStyle.AppName>DevProfile</SigninStyle.AppName>
-        <SigninStyle.Content>
-          <SigninStyle.Title>Faça o seu logon</SigninStyle.Title>
-          <Input placeholder="Email" />
-          <Input placeholder="Senha" />
-          <Button title="Entrar" />
-        </SigninStyle.Content>
-      </SigninStyle.Container>
-    </ScrollView>
+    <>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <SigninStyle.Container>
+          <SigninStyle.Content>
+            <SigninStyle.Logo source={logo} />
+            <SigninStyle.Title>Faça o seu logon</SigninStyle.Title>
+            <Input placeholder="Email" />
+            <Input placeholder="Senha" />
+            <Button title="Entrar" />
+            <SigninStyle.ForgotPasswordButton>
+              <SigninStyle.ForgotPasswordTitle>
+                Esqueci minha senha
+              </SigninStyle.ForgotPasswordTitle>
+            </SigninStyle.ForgotPasswordButton>
+          </SigninStyle.Content>
+        </SigninStyle.Container>
+      </ScrollView>
+      <SigninStyle.CreateAccountButton>
+        <SigninStyle.Icon name="log-in" />
+        <SigninStyle.CreateAccountTitle>
+          Criar uma conta
+        </SigninStyle.CreateAccountTitle>
+      </SigninStyle.CreateAccountButton>
+    </>
   );
 };
--- src/pages/Signin/styles.ts
+++ src/pages/Signin/styles.ts
@@ -1,13 +1,6 @@
 import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
-
-const AppName = styled.Text`
-  font-size: ${RFValue(30)}px;
-  font-family: ${({ theme }) => theme.fonts.regular};
-  color: ${({ theme }) => theme.colors.primary};
-  padding-top: 20px;
-`;
-
+import { Feather } from '@expo/vector-icons';
 const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.dark};
@@ -30,9 +23,56 @@ const Title = styled.Text`
   margin-bottom: 24px;
 `;
 
+const Logo = styled.Image`
+  width: ${RFValue(160)}px;
+  height: ${RFValue(160)}px;
+  margin-bottom: ${RFValue(64)}px;
+`;
+
+const ForgotPasswordButton = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+const ForgotPasswordTitle = styled.Text`
+  font-size: ${RFValue(20)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.gray500};
+`;
+
+const Icon = styled(Feather)`
+  font-size: ${RFValue(20)}px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${({ theme }) => theme.colors.gray800};
+  border-top-width: 1px;
+  border-color: ${({ theme }) => theme.colors.black};
+  padding: 16px 0;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const CreateAccountTitle = styled.Text`
+  font-size: ${RFValue(16)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.primary};
+  margin-left: 16px;
+`;
+
 export const SigninStyle = {
   Container,
   Content,
   Title,
-  AppName,
+  Logo,
+  ForgotPasswordButton,
+  ForgotPasswordTitle,
+  CreateAccountButton,
+  Icon,
+  CreateAccountTitle,
 };
